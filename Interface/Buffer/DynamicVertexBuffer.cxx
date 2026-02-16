16f32c2f25b5520575dc85a4e3699de6c870fa99
EMCJava <loyusum246807952@gmail.com>
2026-02-17 02:50:11 +0800

    DynamicVertexBuffer

---
+++ Interface/Buffer/DynamicVertexBuffer.cxx
@@ -0,0 +1,43 @@
+//
+// Created by LYS on 2/17/2026.
+//
+
+#include <utility>
+
+#include "DynamicVertexBuffer.hxx"
+
+#include <Interface/WindowBase.hxx>
+
+CDynamicVertexBuffer::CDynamicVertexBuffer(CWindowBase* Window, const size_t BytePerElement)
+    : m_BytePerElement(BytePerElement)
+    , m_Window(Window)
+{
+}
+
+void* CDynamicVertexBuffer::PushVoidUninitialized()
+{
+    if (++m_EffectiveBufferSize > m_LogicalBufferSize) {
+        m_LogicalBufferSize = m_EffectiveBufferSize;
+
+        wgpu::BufferDescriptor bufferDesc;
+
+        bufferDesc.label = "DynamicVertexBuffer";
+        bufferDesc.size = m_LogicalBufferSize * m_BytePerElement;
+        bufferDesc.usage = wgpu::BufferUsage::CopyDst | wgpu::BufferUsage::Vertex;
+
+        m_RenderBuffer = m_Window->GetDevice().CreateBuffer(&bufferDesc);
+        m_Data.resize(bufferDesc.size);
+    }
+
+    return m_Data.data() + (m_EffectiveBufferSize - 1) * m_BytePerElement;
+}
+
+CDynamicVertexBuffer::~CDynamicVertexBuffer()
+{
+    auto a = 0;
+}
+
+void CDynamicVertexBuffer::Upload(const std::size_t Offset, const std::size_t Count) noexcept
+{
+    m_Window->GetQueue().WriteBuffer(m_RenderBuffer, Offset * m_BytePerElement, m_Data.data(), Count * m_BytePerElement);
+}
---
+++ Interface/Buffer/DynamicVertexBuffer.hxx
@@ -0,0 +1,47 @@
+//
+// Created by LYS on 2/17/2026.
+//
+
+#pragma once
+
+#include <dawn/webgpu_cpp.h>
+
+#include <memory>
+
+class CDynamicVertexBuffer {
+
+    CDynamicVertexBuffer(class CWindowBase* Window, size_t BytePerElement);
+
+    void* PushVoidUninitialized();
+
+public:
+    ~CDynamicVertexBuffer();
+
+    template <typename Ty>
+    static auto Create(CWindowBase* Window)
+    {
+        return std::unique_ptr<CDynamicVertexBuffer> { new CDynamicVertexBuffer { Window, sizeof(Ty) } };
+    }
+
+    template <typename Ty = void>
+    Ty* PushUninitialized()
+    {
+        return static_cast<Ty*>(PushVoidUninitialized());
+    }
+
+    void Upload(std::size_t Offset, std::size_t Count = 1) noexcept;
+
+    [[nodiscard]] auto GetBufferSize() const noexcept { return m_EffectiveBufferSize; }
+    [[nodiscard]] auto GetBufferByteSize() const noexcept { return m_EffectiveBufferSize * m_BytePerElement; }
+    [[nodiscard]] operator const wgpu::Buffer&() const& noexcept { return m_RenderBuffer; }
+
+protected:
+    uint64_t m_BytePerElement;
+    uint64_t m_EffectiveBufferSize = 0;
+    uint64_t m_LogicalBufferSize = 0;
+
+    wgpu::Buffer m_RenderBuffer;
+    std::vector<uint8_t> m_Data;
+
+    CWindowBase* m_Window;
+};
