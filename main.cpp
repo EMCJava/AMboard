a30fee62f11c238e2fd61e8a8de2029ec822468b
EMCJava <loyusum246807952@gmail.com>
2026-03-17 21:08:16 +0800

    Remove unused include

--- main.cpp
+++ main.cpp
@@ -1,16 +1,6 @@
 #include <Interface/Pipline/DepthTexture.hxx>
-#include <Interface/Pipline/RenderPipeline.hxx>
-
-#include <Interface/Font/TextRenderSystem.hxx>
 
 #include <AMboard/Editor/BoardEditor.hxx>
-
-#include <AMboard/Macro/DataPin.hxx>
-#include <AMboard/Macro/ExecuteNode.hxx>
-#include <AMboard/Macro/FlowPin.hxx>
-
-#include <chrono>
-#include <iostream>
 
 int main()
 {
